c9bd4af8fe921ce9c469fd3280a83396f64b667b
Oberdan Bitencourt Ferreira <oberdan.bitencourt@arctouch.com>
2015-06-02 18:27:57 -0300

    Adding more stuff
    DateTimeHelper, CurrencyHelper, making ServicesPage a demo page
    (textblock rebind not working)

---
+++ PrismApp/Util/GlobalizationHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Globalization.DateTimeFormatting;
+using Windows.Globalization.NumberFormatting;
+
+namespace PrismApp.Util
+{
+    internal class DateTimeFormat
+    {
+        public static string ToShortDateTimeFormat(DateTime datetime, string timezone = null)
+        {
+            return string.Format("{0} {1}", ToShortDateFormat(datetime, timezone), ToShortTimeFormat(datetime, timezone));
+        }
+
+        public static string ToLongtDateTimeFormat(DateTime datetime, string timezone = null)
+        {
+            return string.Format("{0} {1}", ToLongDateFormat(datetime, timezone), ToLongTimeFormat(datetime, timezone));
+        }
+
+        public static string ToShortDateFormat(DateTime datetime, string timezone = null)
+        {
+            return String.IsNullOrEmpty(timezone)
+                ? DateTimeFormatter.ShortDate.Format(datetime)
+                : DateTimeFormatter.ShortDate.Format(datetime, timezone);
+        }
+
+        public static string ToShortTimeFormat(DateTime datetime, string timezone = null)
+        {
+            return String.IsNullOrEmpty(timezone)
+                ? DateTimeFormatter.ShortTime.Format(datetime)
+                : DateTimeFormatter.ShortTime.Format(datetime, timezone);
+        }
+
+        public static string ToLongDateFormat(DateTime datetime, string timezone = null)
+        {
+            return String.IsNullOrEmpty(timezone)
+                ? DateTimeFormatter.LongDate.Format(datetime)
+                : DateTimeFormatter.LongDate.Format(datetime, timezone);
+        }
+
+        public static string ToLongTimeFormat(DateTime datetime, string timezone = null)
+        {
+            return String.IsNullOrEmpty(timezone)
+                ? DateTimeFormatter.LongTime.Format(datetime)
+                : DateTimeFormatter.LongTime.Format(datetime, timezone);
+        }
+    }
+
+    internal class Currency
+    {
+        private static readonly string USD = "USD";
+
+        public static string ToUSD(double fractionalNumber)
+        {
+            return Format(USD, fractionalNumber);
+        }
+
+        public static string Format(string currencyCode, double fractionalNumber)
+        {
+            return (new CurrencyFormatter(currencyCode)).Format(fractionalNumber);
+        }
+    }
+}
--- PrismApp/ViewModels/ServicesPageViewModel.cs
+++ PrismApp/ViewModels/ServicesPageViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Practices.Prism.StoreApps;
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Microsoft.Practices.Prism.StoreApps;
 using Microsoft.Practices.Prism.StoreApps.Interfaces;
 
 namespace PrismApp.ViewModels
@@ -7,11 +9,38 @@ namespace PrismApp.ViewModels
     {
         private INavigationService _navigationService;
 
+        public string ShortDateTimeNow { get; set; }
+
+        public string LongDateTimeNow { get; set; }
+
+        public ComboBoxItem CurrencySelectedObject { get; set; }
+
+        public string CurrencyCode { get { return CurrencySelectedObject.Content as string; } }
+
         public string Service { get { return "Service"; } }
+
+        public DelegateCommand CurrencySelectionChangedCommand { get; set; }
+
+        public string FormattedCurrency { get; private set; }
 
         public ServicesPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            CurrencySelectionChangedCommand = new DelegateCommand(CurrencySelectionChanged);
+            var now = DateTime.Now;
+            ShortDateTimeNow = Util.DateTimeFormat.ToShortDateTimeFormat(now);
+            LongDateTimeNow = Util.DateTimeFormat.ToLongtDateTimeFormat(now);
+        }
+
+        private void CurrencySelectionChanged()
+        {
+            FormattedCurrency = (string.IsNullOrEmpty(CurrencyCode))
+                ? string.Empty
+                : Util.Currency.Format(CurrencyCode, 2380.97d);
+        }
+
+        public ServicesPageViewModel()
+        {
         }
     }
 }
